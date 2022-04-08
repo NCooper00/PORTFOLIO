c7341577adbbe3112b21dcd479327798402c4471
Nick Cooper <92492572+NCooper00@users.noreply.github.com>
2022-04-08 05:33:38 -0700

    Added resume

--- script.js
+++ script.js
@@ -1,20 +1,33 @@
 const submit = document.querySelector('.sendIdea');
 const sunLoad = document.querySelector('.sunLoad');
+const success = document.querySelector('.success');
 
-submit.addEventListener('click', () => {
-    var timeLeft = 2;
-    const loadStatus = sunLoad.getAttribute('data-visible');
-    if (loadStatus === "false") {
-        sunLoad.setAttribute('data-visible', true);
-        var timer = setInterval(function() {
-            if (timeLeft < 0) {
-                clearInterval(timer);
-                sunLoad.setAttribute('data-visible', false);
-            } else {
-                timeLeft--;
-            }
-        }, 1000)
+const CName = document.getElementById('clientName');
+const CEmail = document.getElementById('clientEmail');
+const CIdea = document.getElementById('clientIdea');
+
+submit.addEventListener('click', (event) => {
+    if (CName.textContent && CEmail.textContent && CIdea.textContent) {
+       var timeLeft = 2;
+        const loadStatus = sunLoad.getAttribute('data-visible');
+        console.log();
+        if (loadStatus === "false") {
+            sunLoad.setAttribute('data-visible', true);
+            success.style.opacity = "100%"
+            var timer = setInterval(function() {
+                if (timeLeft < 0) {
+                    clearInterval(timer);
+                    sunLoad.setAttribute('data-visible', false);
+                    success.style.opacity = "0"
+                } else {
+                    timeLeft--;
+                }
+            }, 1000)
+        } 
+    } else {
+        return;
     }
+    
 })
 
 const previous = document.querySelector('.previous');
